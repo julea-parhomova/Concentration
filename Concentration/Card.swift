d4be88476dcc796569b94693e6e4aab4b63a56dd
Julea Parkhomava <juleaparkhomava@Mac-Julea.local>
2021-02-23 18:10:56 +0300

    Concentration game

---
+++ Concentration/Card.swift
@@ -0,0 +1,40 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Julea Parkhomava on 2/22/21.
+//
+
+import Foundation
+
+struct Card: Hashable{
+    var isMatched: Bool = false
+    var isFaceUp: Bool = false
+    private var identifier: Int
+    
+    static var identifierFactory: Int = 0
+    
+    static func getUniqueIdentifier() -> Int{
+        identifierFactory += 1
+        return identifierFactory
+    }
+    
+    init(){
+        identifier = Card.getUniqueIdentifier()
+    }
+    
+    init(m: Bool, f: Bool, i: Int){
+        isMatched = m
+        isFaceUp = f
+        identifier = i
+    }
+    
+    static func ==(first: Card, second: Card) -> Bool{
+        first.identifier == second.identifier
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(identifier)
+    }
+    
+}
